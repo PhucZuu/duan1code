4e73df146558abc7c1a118814ab1c929f7a331a9
Hoaquynh58 <quynhhhph44910@fpt.edu.vn>
2024-03-19 16:09:04 +0700

    update code

--- admin/danhmuc/update.php
+++ admin/danhmuc/update.php
@@ -1,1 +1,30 @@
-<?php?>
+<div class="tabs__content">
+    <div class="tab__content active-tab">
+    <h3 class="tab__header">CẬP NHẬT LOẠI HÀNG HÓA</h3>
+    <?php
+        if(is_array($dm)){
+            // print_r($dm);
+            extract($dm);
+        }
+    ?>
+    <div class="register">
+        <form action="index.php?act=updatedm" class="form grid" method="post">
+            Mã loại <br>
+            <input type="text" placeholder="Auto number" class="form__input" name="id_danh_muc">
+            Tên loại <br>
+            <input type="text" placeholder="Nhập vào tên" class="form__input" name="ten_danh_muc" value="<?php if(isset($ten_danh_muc) && $ten_danh_muc!="") echo $ten_danh_muc ?>">
+            <div class="form__btn">
+                <input type="hidden" name="id_danh_muc" value="<?php if(isset($id_danh_muc) && $id_danh_muc>0) echo $id_danh_muc ?>">
+                <input class="btn" type="submit" name="capnhat" value="CẬP NHẬT">
+                <input class="btn" type="reset" value="NHẬP LẠI">
+                <a href="index.php?act=listdm"><input class="btn" type="button" value="DANH SÁCH"></a>
+            </div>
+            <?php
+                if(isset($thongbao) && ($thongbao != "")){
+                    echo '<h2 style="color:green">'.$thongbao.'</h2>';
+                }
+            ?>
+        </form>
+    </div>
+    </div>
+</div>
--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,80 @@
 <?php
     include_once 'headeradmin.php';
     include_once 'boxdanhmuc.php';
-    include_once 'homeadmin.php';
+    if(isset($_GET["act"])){
+        $act = $_GET["act"];
+        switch ($act){
+            case 'adddm':
+                //kiểm tra xem người dùng có click vào nút add hay không
+                if(isset($_POST['themmoi'])&&($_POST['themmoi'])){
+                    $ten_danh_muc = $_POST['ten_danh_muc'];
+
+                    insert_danhmuc($ten_danh_muc);
+
+                    //thông báo đã thực thi xong
+                    $thongbao = "Thêm thành công";
+                }
+                include "danhmuc/add.php";
+                break;
+
+                case 'listdm':
+                    $listdanhmuc = loadAll_danhmuc();
+                    include "danhmuc/list.php";
+                    break;
+
+                case 'xoadm':
+                    //kiểm tra id có tồn tại ko để xóa
+                    if(isset($_GET['id_danh_muc'])&&(($_GET['id_danh_muc'])>0)){
+                        $id_danh_muc = $_GET['id_danh_muc'];
+                        // echo $id_danh_muc;
+                        delete_danhmuc($id_danh_muc);
+
+                    }
+
+                    // trong file pdo phần này có return kết quả trả về nền ta cần gán về 1 giá trị
+                    $listdanhmuc = loadAll_danhmuc();
+                    include "danhmuc/list.php";
+                    break;
+
+                case 'suadm':
+                    //kiểm tra id có tồn tại ko để xóa
+                    if(isset($_GET['id_danh_muc'])&&(($_GET['id_danh_muc'])>0)){
+                        $id_danh_muc = $_GET['id_danh_muc'];
+                        // echo $id_danh_muc;
+                        // die();
+                        $dm = loadone_danhmuc($id_danh_muc);
+                    }
+                    include "danhmuc/update.php";
+                    break;
+
+                case 'updatedm':
+                    //làm như phần act ở trên
+                    if(isset($_POST['capnhat'])&&($_POST['capnhat'])){
+                        $id_danh_muc = $_POST['id_danh_muc'];
+                        $ten_danh_muc = $_POST['ten_danh_muc'];
+
+                        // echo '<pre>';
+                        // print_r([$id_danh_muc,$ten_danh_muc]);
+                        // echo '</pre>';
+
+
+                        update_danhmuc($id_danh_muc,$ten_danh_muc);
+                        //thông báo đã thực thi xong
+                        $thongbao = "Cập nhật thành công";
+                    }
+                    $listdanhmuc = loadAll_danhmuc();
+                    
+                    include "danhmuc/list.php";
+                    break;
+        }
+    }
+    else{
+        include "homeadmin.php";
+    }
     include_once 'footeradmin.php';
+    include_once '../models/pdo.php';
+    include_once '../models/danhMuc.php';
+
+
+    
 ?>
