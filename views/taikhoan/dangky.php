f305f2c77a3d1e7bc2fc7392a5e14242cc13d266
PhucZuu <phucdinhle2004@gmail.com>
2024-04-01 20:48:38 +0700

    update css login logout

--- views/taikhoan/dangky.php
+++ views/taikhoan/dangky.php
@@ -1,5 +1,5 @@
 <section class="login-register section--lg">
-  <div class="login-register__container container grid">
+  <div class="login-register__container formlogin container">
     <!-- FORM ĐĂNG KÝ -->
     <div class="register">
       <h3 class="section__title">Đăng ký</h3>
@@ -23,8 +23,8 @@
         <input type="text" placeholder="Địa chỉ" class="form__input" name="dia_chi" value="<?= $dia_chi ?>">
         <span style="color:red"><?= $errDiaChi ?></span>
 
-        <div class="form__btn">
-          <input type="submit" class="btn" value="Đăng ký" name="dangky">
+        <div class="form__btn btnlogin">
+          <input type="submit" class="btn btnw" value="Đăng ký" name="dangky">
         </div>
       </form><br>
       <h2 class="thongbao">
